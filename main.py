b426b4ad592d69e1c81640e523fca533ad37a32d
Serge Terryn <serge@debian.telenet.be>
2024-03-09 13:48:33 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import sys
+import form
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QFileDialog
+
+
+class Window(QMainWindow, form.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.center()
+        self.btnScanDir.clicked.connect(self.openDirectory)
+        self.btnChangePermissions.clicked.connect(self.setPermissions)
+        self.dir_mode = ""
+        self.file_mode = ""
+        self.startpath = ""
+        self.needSudo = False
+        
+        
+    def list_files_and_directories(self):
+        for root, dirs, files in os.walk(self.startpath, topdown=False):
+            for name in files:
+                f = (os.path.join(root, name))
+                if self.needSudo:
+                    cmd = 'sudo chmod ' + self.file_mode + ' ' + f
+                else:
+                    cmd = 'chmod ' + self.file_mode + ' ' + f
+                os.system(cmd)
+            for name in dirs:
+                d = (os.path.join(root, name))
+                if self.needSudo:
+                    cmd = 'sudo chmod ' + self.dir_mode + ' ' + d
+                else:
+                    cmd = 'chmod ' + self.dir_mode + ' ' + d
+
+        print("Done !")
+        exit(0)
+
+    def setPermissions(self):
+        self.dir_mode = self.lineEditDirPermissions.text()
+        self.file_mode = self.lineEditFilePermissions.text()
+        if self.checkBoxSudo.checked():
+            self.needSudo = True
+        self.list_files_and_directories()
+
+    def openDirectory(self):
+        file = QFileDialog.getExistingDirectory(self, "Select Directory")
+        self.lineEdit.setText(file)
+        self.startpath = file
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeApp(self):
+        exit(0)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
